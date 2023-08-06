5d84b0121f8ac12a114dbdd2d57b46051254b2a3
Birkan Karakaya <birkan.karakayaa@gmail.com>
2023-08-06 14:52:57 +0300

    First Repository written and dapper installed

---
+++ RealEstate_Dapper_Api/Models/DapperContext/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RealEstate_Dapper_Api.Models.DapperContext
+{
+    public class Context
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connnectionString;
+
+        public Context(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connnectionString = _configuration.GetConnectionString("connection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connnectionString);
+
+    }
+}
