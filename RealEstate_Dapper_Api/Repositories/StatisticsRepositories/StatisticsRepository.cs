141afaf4ad1d7bca71a8e0315fe8eef8eff2c2d5
Birkan Karakaya <birkan.karakayaa@gmail.com>
2023-12-18 15:22:34 +0300

    Statistics controller started

---
+++ RealEstate_Dapper_Api/Repositories/StatisticsRepositories/StatisticsRepository.cs
@@ -0,0 +1,105 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.StatisticsRepositories
+{
+    public class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly Context _context;
+
+        public StatisticsRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public int ActiveCategoryCount()
+        {
+            string query = "SELECT COUNT(*) FROM Category WHERE CategoryStatus = 1";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int ActiveEmployeeCount()
+        {
+            string query = "SELECT COUNT(*) FROM Employee WHERE Status = 1";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = connection.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int ApartmentCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal AvarageProductByRent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal AvarageProductBySale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AvarageRoomCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CategoryNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CityNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DifferentCityCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EmployeeNameByMaxProductCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal LastProductPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewestBuildingYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string OldestBuildingYear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PassiveCategoryCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProductCount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
